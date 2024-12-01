2aa5aa02b828462de5c9c4a015f2d3346555b245
Christiano Bourguignon <christianombl@gmail.com>
2024-12-01 20:20:54 -0300

    Remoção de comentário

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,3 @@
-//[Prompt] O programa Prompt armazena uma lista de nomes.
-//Como o próprio nome sugere, ele recebe comandos por um
-// linha
-//no console após o prompt >. Existem os seguintes
-// comandos: (+)
-//insere o nome recebido como argumento na coleção; (-)
-// exclui
-//o primeiro nome encontrado da coleção; (L) gera a lista
-// de nomes
-//armazenados. Utilize a coleção mais adequada e de
-// melhor performance.
-//Exemplos de comandos:
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
